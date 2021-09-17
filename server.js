398af94eb8fb472fdc9b28e7c709efd797a12119
Krishna Kushwaha <75431419+krishna-kush@users.noreply.github.com>
2021-09-17 14:32:16 +0530

    not upload to server
    multer not running
    cuz serverless

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const multer = require('multer'); // for multer use the specified methods only, like in react...
+// const fs = require('fs');
+// const multer = require('multer'); // for multer use the specified methods only, like in react...
+
 
 
 const mongoose = require('mongoose')
@@ -15,16 +16,16 @@ mongoose.connect("mongodb+srv://main-PC:mymongodb.server@base.dmmx9.mongodb.net/
 })
 
 
-FOR IMAGE UPLOADATION USING MULTER
+// // FOR IMAGE UPLOADATION USING MULTER
 
 
-// Setting The Storage Engine
-const storage = multer.diskStorage({
-    destination: './public/book_images/',
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)) // cd first argument is err -> seted to null
-    }
-})
+// // Setting The Storage Engine
+// const storage = multer.diskStorage({
+//     destination: './public/book_images/',
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)) // cd first argument is err -> seted to null
+//     }
+// })
 // // Check File Type -> Coppied from bradtraversy/nodeuploads
 // const checkFileType = (file, cb) => {
 //     // Allowed ext
@@ -42,14 +43,14 @@ const storage = multer.diskStorage({
 // }
 
 
-// init upload
-const Upload = multer({
-    storage: storage,
-    limits: {fileSize: 10000000} // 10 mb in bites (take in bites)
+// // init upload
+// const Upload = multer({
+//     storage: storage,
+//     limits: {fileSize: 10000000}, // 10 mb in bites (take in bites)
 //     fileFilter: (req, file, cb) => {
 //         checkFileType(file, cb)
 //     }
-}).single('book_image') // single or multipple -> take filed name as parameter to upload/point given file
+// }).single('book_image') // single or multipple -> take filed name as parameter to upload/point given file
 
 
 
@@ -72,34 +73,34 @@ server.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 })
 
-server.post("/upload", (req, res) => {
+// server.post("/upload", (req, res) => {
     // console.log("hi");
-    Upload(req, res, (err) => { // why only this format
-        console.log(req.file); // only can access res.file inside Upload(inside multer component) cuz express has no way to handle images of that I know...
+    // Upload(req, res, (err) => { // why only this format
+    //     console.log(req.file); // only can access res.file inside Upload(inside multer component) cuz express has no way to handle images of that I know...
 
-        const params = {
-            dest: req.file.path
-        }
+    //     const params = {
+    //         dest: req.file.path
+    //     }
 
 
-        if (err) {
-            // res.sendFile(path.resolve(__dirname,'views','info.html'), params)
-            res.send(params)
-            // console.log(params);
+    //     if (err) {
+    //         // res.sendFile(path.resolve(__dirname,'views','info.html'), params)
+    //         res.send(params)
+    //         // console.log(params);
             
-        }else {
-            // console.log(params);
-            res.send(params)
-        }
-    })
+    //     }else {
+    //         // console.log(params);
+    //         res.send(params)
+    //     }
+    // })
 
-})
+// })
 
 server.post("/finalise", async (req, res) => {
     // console.log(req.body);
     
-    let a = fs.readFileSync(req.body.dest)
-    let b = a.toString('base64')
+    // let a = fs.readFileSync(req.body.dest)
+    // let b = a.toString('base64')
     
     let Record = (disc) => {
         if (disc) {
@@ -107,17 +108,17 @@ server.post("/finalise", async (req, res) => {
                 Client_Name: req.body.username,
                 Book_Name: req.body.book_name,
                 Price: req.body.price,
-                Book_Image: b,
+                Book_Image: req.body.img,
                 Discription: disc,
-                Name_at_Server: req.body.dest.slice(19, req.body.dest.length-1)
+                // Name_at_Server: req.body.dest.slice(19, req.body.dest.length-1)
             }
         }else {
             return {
                 Client_Name: req.body.username,
                 Book_Name: req.body.book_name,
                 Price: req.body.price,
-                Book_Image: b,
-                Name_at_Server: req.body.dest.slice(19, req.body.dest.length-1)
+                Book_Image: req.body.img,
+                // Name_at_Server: req.body.dest.slice(19, req.body.dest.length-1)
             }
         }
     }
@@ -128,3 +129,15 @@ server.post("/finalise", async (req, res) => {
     res.send("DONE")
 
 })
+
+// server.get("/upload1", async (req, res) => {
+//     await fetch("https://ml-for-rent-my-book-default-rtdb.firebaseio.com/first.json", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             img: ""
+//         })
+//     })
+// })
