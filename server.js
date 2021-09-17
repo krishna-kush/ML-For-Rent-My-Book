fca81d46dfb7edce622a086bec85696257561912
Krishna Kushwaha <75431419+krishna-kush@users.noreply.github.com>
2021-09-17 14:44:09 +0530

    cleaning junk

--- server.js
+++ server.js
@@ -1,143 +1,52 @@
-const express = require('express');
-const path = require('path');
-// const fs = require('fs');
-// const multer = require('multer'); // for multer use the specified methods only, like in react...
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
 
 
+export default () => {
+    const [img, setImg] = useState("")
 
-const mongoose = require('mongoose')
-
-const ML = require('./models/form_model.js');
-
-
-mongoose.connect("mongodb+srv://main-PC:mymongodb.server@base.dmmx9.mongodb.net/Main?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    let ImgChanged = (event) => {
+        let img_file = event.target.files[0] // array of files uploaded...
+        console.log(img_file);
 
 
-// // FOR IMAGE UPLOADATION USING MULTER
+        let ConvertImg = (file) => {
+            let reader = new FileReader()
 
-
-// // Setting The Storage Engine
-// const storage = multer.diskStorage({
-//     destination: './public/book_images/',
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)) // cd first argument is err -> seted to null
-//     }
-// })
-// // Check File Type -> Coppied from bradtraversy/nodeuploads
-// const checkFileType = (file, cb) => {
-//     // Allowed ext
-//     const filetypes = /jpeg|jpg|png/;
-//     // Check ext
-//     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-//     // Check mime
-//     const mimetype = filetypes.test(file.mimetype);
-  
-//     if(mimetype && extname){
-//         return cb(null,true);
-//     } else {
-//         cb('Error: Images Only!');
-//     }
-// }
-
-
-// // init upload
-// const Upload = multer({
-//     storage: storage,
-//     limits: {fileSize: 10000000}, // 10 mb in bites (take in bites)
-//     fileFilter: (req, file, cb) => {
-//         checkFileType(file, cb)
-//     }
-// }).single('book_image') // single or multipple -> take filed name as parameter to upload/point given file
-
-
-
-const port = 5000;
-
-const server = express();
-
-server.use(express.urlencoded())
-
-server.listen(port, () =>{
-    console.log(`Server running at ${port}/`);
-})
-
-
-
-
-
-server.get("/", (req, res) => {
-    server.use(express.static(path.resolve(__dirname,'client','build')))
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-})
-
-// server.post("/upload", (req, res) => {
-    // console.log("hi");
-    // Upload(req, res, (err) => { // why only this format
-    //     console.log(req.file); // only can access res.file inside Upload(inside multer component) cuz express has no way to handle images of that I know...
-
-    //     const params = {
-    //         dest: req.file.path
-    //     }
-
-
-    //     if (err) {
-    //         // res.sendFile(path.resolve(__dirname,'views','info.html'), params)
-    //         res.send(params)
-    //         // console.log(params);
+            reader.readAsDataURL(file)
             
-    //     }else {
-    //         // console.log(params);
-    //         res.send(params)
-    //     }
-    // })
-
-// })
-
-server.post("/finalise", async (req, res) => {
-    // console.log(req.body);
-    
-    // let a = fs.readFileSync(req.body.dest)
-    // let b = a.toString('base64')
-    
-    let Record = (disc) => {
-        if (disc) {
-            return {
-                Client_Name: req.body.username,
-                Book_Name: req.body.book_name,
-                Price: req.body.price,
-                Book_Image: req.body.img,
-                Discription: disc,
-                // Name_at_Server: req.body.dest.slice(19, req.body.dest.length-1)
-            }
-        }else {
-            return {
-                Client_Name: req.body.username,
-                Book_Name: req.body.book_name,
-                Price: req.body.price,
-                Book_Image: req.body.img,
-                // Name_at_Server: req.body.dest.slice(19, req.body.dest.length-1)
+            reader.onload = () => {
+                setImg(reader.result)
             }
         }
+
+        ConvertImg(img_file)
     }
 
 
-    await ML.create(Record(req.body.disc))
+    return (
+        <div>
+            <div class="img">
+                <label>Book Image (Under 10mb): </label>
+                <input
+                type="file"
+                name="book_image"
+                onChange={ImgChanged}
+                required/>
 
-    res.send("DONE")
-
-})
-
-// server.get("/upload1", async (req, res) => {
-//     await fetch("https://ml-for-rent-my-book-default-rtdb.firebaseio.com/first.json", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//             img: ""
-//         })
-//     })
-// })
+                {/* <button onClick={() => {
+                    UpImg('book_image')
+                }}>Upload Img</button> */}
+            </div>
+            
+            <Link to={
+                {
+                    pathname: '/mid',
+                    state: img
+                }
+            }>
+                <button type="submit">Next</button>
+            </Link>
+        </div>
+    )
+}
